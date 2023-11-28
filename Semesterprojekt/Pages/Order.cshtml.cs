fe2ca95943c4c8bb3049e60966db68e0b038bb5e
Becky-B-Lau <Bblau@outlook.dk>
2023-11-28 12:23:33 +0100

    .

---
+++ Semesterprojekt/Pages/Order.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Semesterprojekt.Pages
+{
+    public class OrderModel : PageModel
+    {
+        private readonly ILogger<OrderModel> _logger;
+
+        public OrderModel(ILogger<OrderModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
