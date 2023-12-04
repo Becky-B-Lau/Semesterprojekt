091d9e5dd760feb2923726fe1c39c4815c519e46
Clara Neu <mariaclara.neu@gmail.com>
2023-12-04 10:58:56 +0100

    Added Order Class & Orderoversigt Tabel

---
+++ Semesterprojekt/Models/Ordre.cs
@@ -0,0 +1,31 @@
+﻿namespace Semesterprojekt.Models
+{
+	public class Ordre
+	{
+		private int Id { get; set; }
+		private DateTime DateTime { get; set; }
+		public Kunde Kunde { get; set; }
+		private string Beskrivelse { get; set; }
+		private double Pakke { get; set; }
+		private bool Godkendt { get; set; }
+
+		public Ordre() { }
+
+		public Ordre(int id, DateTime datetime, Kunde kunde, string beskrivelse, double pakke, bool godkendt)
+		{
+			Id = id++;
+			DateTime = datetime;
+			Kunde = kunde;
+			Beskrivelse = beskrivelse;
+			Pakke = pakke;
+			Godkendt = godkendt;
+		}
+
+		//UpdateOrdre
+
+		//DeleteOrdre
+	}
+
+
+}
+
