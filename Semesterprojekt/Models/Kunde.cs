36c363180b13a92a0b13c3d42bea13aa46e04646
AmalieLindberg <aml001@edu.zealand.dk>
2023-12-05 11:07:06 +0100

    Tilføjet metoder til kunde

--- Semesterprojekt/Models/Kunde.cs
+++ Semesterprojekt/Models/Kunde.cs
@@ -3,6 +3,8 @@
 
     public class Kunde : Person
     {
+        private static List<Kunde> kunder = new List<Kunde>();
+
         private int Id { get; set; }
         public Kunde() : base()
         {
@@ -12,6 +14,44 @@
 
             Id = id;
         }
+        public static void AddKunde(Kunde kunde)
+        {
+            kunder.Add(kunde);
+        }
+        public static void AddKunde( int id, string navn, int alder, int telefonnummer, string adresse, string email)
+        {
+            kunder.Add(new Kunde(id, navn, alder, telefonnummer, adresse, email));
+        }
 
+        public static Kunde? RemoveKundeVedNavn(string navn)
+        {
+            foreach (Kunde kunde in kunder)
+            {
+                if (kunde.Navn == navn)
+                {
+                    kunder.Remove(kunde);
+                    return kunde;
+                }
+            }
+
+            return null;
+        }
+
+        public static List<Kunde> GetKunder()
+        {
+            return kunder;
+        }
+
+
+        public static Kunde? SearchKundeVedNavn(string navn)
+        {
+            foreach (Kunde kunde in kunder)
+            {
+                if (kunde.Navn == navn)
+                    return kunde;
+            }
+
+            return null;
+        }
     }
 }
