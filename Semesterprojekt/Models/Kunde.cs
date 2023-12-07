b823f5b5b077dc66b38140ef5a368bfbf7528d24
AmalieLindberg <aml001@edu.zealand.dk>
2023-12-07 09:55:55 +0100

    slettet kunde

--- Semesterprojekt/Models/Kunde.cs
+++ Semesterprojekt/Models/Kunde.cs
@@ -3,15 +3,12 @@
 
     public class Kunde : Person
     {
-        private static List<Kunde> kunder = new List<Kunde>();
-
         private int Id { get; set; }
         public Kunde() : base()
         {
         }
         public Kunde(int id, string navn, int alder, int telefonnummer, string adresse, string email) : base(navn, alder, telefonnummer, adresse, email)
         {
-
             Id = id;
         }
     }
