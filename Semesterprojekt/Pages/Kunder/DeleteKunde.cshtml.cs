efbdb03622993d9285de52b994dd2a045b511e01
AmalieLindberg <aml001@edu.zealand.dk>
2023-12-07 14:01:40 +0100

    Kunde flyttet

--- Semesterprojekt/Pages/Kunder/DeleteKunde.cshtml.cs
+++ Semesterprojekt/Pages/Kunder/DeleteKunde.cshtml.cs
@@ -2,4 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Semesterprojekt.Pages.Kunder
-{ }
+{
+	public class DeleteKundeModel : PageModel
+	{
+		public void OnGet()
+		{
+		}
+
+	}
+}
--- Semesterprojekt/Pages/Kunder/GetAllKunder.cshtml.cs
+++ Semesterprojekt/Pages/Kunder/GetAllKunder.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Semesterprojekt.Service;
+using Semesterprojekt.MockData;
 
 namespace Semesterprojekt.Pages.Kunder
 {
-    public class GetAllKunderModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class GetAllKunderModel : PageModel
+	{
+
+		private IItemService _itemService;
+
+		public GetAllKunderModel(IItemService itemService) // Dependency Injection
+		{
+			_itemService = itemService;
+		}
+
+		public List<Models.Ordre>? Items { get; private set; }
+
+		public void OnGet()
+		{
+			Items = _itemService.GetItems();
+		}
+	}
 }
