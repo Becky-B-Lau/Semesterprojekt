83ebe430c3f5a0498bbdf0b539da1e1a8547ed19
AmalieLindberg <aml001@edu.zealand.dk>
2023-12-07 10:03:58 +0100

    Irriterende merge koder fjernet

--- Semesterprojekt/Models/Kunde.cs
+++ Semesterprojekt/Models/Kunde.cs
@@ -3,26 +3,19 @@
 
     public class Kunde : Person
     {
-<<<<<<< HEAD
         private int Id { get; set; }
-=======
-       public bool Erhverv { get; set; }
+        public bool Erhverv { get; set; }
         public bool Privat { get; set; }
         public int Kundeid { get; }
         private static int KundeId = 1;
->>>>>>> ee1dd6723bad7ee1792d74a743afe8a3c3b9ff1d
         public Kunde() : base()
         {
         }
         public Kunde(string navn, int alder, int telefonnummer, string adresse, string email, bool erhverv, bool privat) : base(navn, alder, telefonnummer, adresse, email)
         {
-<<<<<<< HEAD
-            Id = id;
-=======
             Erhverv = erhverv;
             Privat = privat; 
             Kundeid = KundeId++;
->>>>>>> ee1dd6723bad7ee1792d74a743afe8a3c3b9ff1d
         }
     }
 }
