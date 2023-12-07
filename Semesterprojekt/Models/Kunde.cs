1bebc30078bbc3200f5bceb1a05fd678c8b56f06
Maria <mariadybdahl@hotmail.dk>
2023-12-07 13:37:55 +0100

    Update Kunde.cs

--- Semesterprojekt/Models/Kunde.cs
+++ Semesterprojekt/Models/Kunde.cs
@@ -3,22 +3,18 @@
 
     public class Kunde : Person
     {
-<<<<<<< HEAD
-   //ja
-=======
-        public bool Erhverv { get; set; }
-        public bool Privat { get; set; }
->>>>>>> 5761968fa8067d3dd01bd7f9fd77101b63473544
+
+
+        public string Type { get; set; }
         public int Kundeid { get; }
         private static int KundeId = 1;
         public Kunde() : base()
         {
         }
 
-        public Kunde(string navn, int alder, int telefonnummer, string adresse, string email, bool erhverv, bool privat) : base(navn, alder, telefonnummer, adresse, email)
+        public Kunde(string navn, int alder, int telefonnummer, string adresse, string email, string type) : base(navn, alder, telefonnummer, adresse, email)
         {
-            Erhverv = erhverv;
-            Privat = privat; 
+            Type = type;
             Kundeid = KundeId++;
         }
     }
