c1243bd1af880a6ba6c86816a0e88c54cb22f4b6
AmalieLindberg <aml001@edu.zealand.dk>
2023-12-08 10:30:53 +0100

    Update DeleteKunde.cshtml.cs

--- Semesterprojekt/Pages/Kunder/DeleteKunde.cshtml.cs
+++ Semesterprojekt/Pages/Kunder/DeleteKunde.cshtml.cs
@@ -1,13 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Semesterprojekt.Models;
+using Semesterprojekt.MockData;
+using Semesterprojekt.Service;
 
 namespace Semesterprojekt.Pages.Kunder
+
 {
-	public class DeleteKundeModel : PageModel
+	public class DeleteKunde : PageModel
 	{
-		public void OnGet()
+		private IItemService _itemService;
+
+		public DeleteKunde(IItemService itemService)
 		{
+			_itemService = itemService;
 		}
 
+		[BindProperty]
+		public Models.Kunde Kunde { get; set; }
+
+
+		public IActionResult OnGet(int id)
+		{
+			Kunde = _itemService.Kunde(id);
+			if (Item == null)
+				return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+
+			return Page();
+		}
+
+		public IActionResult OnPost()
+		{
+			Models.Item deletedItem = _itemService.DeleteItem(Item.Id);
+			if (deletedItem == null)
+				return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+
+			return RedirectToPage("GetAllItems");
+		}
 	}
-}
