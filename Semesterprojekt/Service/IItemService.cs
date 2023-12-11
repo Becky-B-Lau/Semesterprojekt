f5ae2ca2ceaf23bb87b236815affc4cbae4dedb3
AmalieLindberg <aml001@edu.zealand.dk>
2023-12-11 09:41:38 +0100

    .

--- Semesterprojekt/Service/IItemService.cs
+++ Semesterprojekt/Service/IItemService.cs
@@ -4,9 +4,7 @@ namespace Semesterprojekt.Service
 {
 	public interface IItemService
 	{
-		//
 		IEnumerable<Ordre> NameSearch(string str);
-
 		List<Ordre> GetItems();
 		void AddItem(Ordre item);
 		void UpdateItem(int? kundeid);
