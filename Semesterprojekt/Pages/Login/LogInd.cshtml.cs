a1ca971ccf1c07c0f786fd31993fbfcfe28fec79
Maria <mariadybdahl@hotmail.dk>
2023-12-18 09:04:41 +0100

    Kommentarer til Contact, LogInd.cs og LogUd.cs

--- Semesterprojekt/Pages/Login/LogInd.cshtml.cs
+++ Semesterprojekt/Pages/Login/LogInd.cshtml.cs
@@ -11,11 +11,14 @@ namespace Semesterprojekt.Pages.Login
 {
     public class LogIndModel : PageModel
     {
+        //En static property som referere til en User der er logget ind
         public static User LoggedInUser { get; set; } = null;
 
+        //instance field:
         private UserService _userService;
 
-
+        //[BindProperty] bliver brugt når vi skal binde egenskab noget til data under modelbinding. og DataType(DataType.Password) bliver brugt når vi skal binde til et bestemt egenskab
+        //som i dette tilfælde er adgangskoden.
         [BindProperty]
         public string UserName { get; set; }
 
@@ -23,6 +26,7 @@ namespace Semesterprojekt.Pages.Login
         public string Password { get; set; }
 
 
+        //Constructor indeholder parameterne itemService af typen IItemService.
         public LogIndModel(UserService userService)
         {
             _userService = userService;
@@ -30,29 +34,36 @@ namespace Semesterprojekt.Pages.Login
         public void OnGet()
         {
         }
+
+        //async den kan gør det i baggrund og ikke blokere hovedtråden (det er den som er ansvarlig for at køre vores applikations logik).
+        //hvis vi gør uden async kan hovedtråden bliver inaktiv indtil den er færdig hvilket gør at programmet kan virke frosset eller ikke reagerer fordi den venter på at den er færdig.
+
+        //Task<IActionResult> går ude på at Task viser at den skal returnerer en opgave af type IActionResult.
         public async Task<IActionResult> OnPost()
         {
 
-            List<User> users = _userService.users;
-            foreach (User user in users)
+            List<User> users = _userService.users; //Opretter en list af bruger som henter listen af bruger i _userService.users
+            foreach (User user in users) //foreach loop som går i gennem den liste af bruger 
             {
 
-                if (UserName == user.UserName && Password == user.Password)
+                if (UserName == user.UserName && Password == user.Password) //Hvis brugernavnet (angivet af kunde) og er det sammen som brugernavet i listen + adgangskode (angivet af kunde) som er det sammen som adgangskode i listen.
                 {
 
-                    LoggedInUser = user;
+                    LoggedInUser = user; //hvis den finder et match i loopet gennems bruger i LoggedInUser.
 
-                    var claims = new List<Claim> { new Claim(ClaimTypes.Name, UserName) };
+                    var claims = new List<Claim> { new Claim(ClaimTypes.Name, UserName) }; 
 
                     var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
                     await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                    return RedirectToPage("/Index");
+
+                    //fra var claims (l.54) til new ClaimsPrincipal(claimsIdentity)); (l. 57): Programmet oprette en brugeridentitet og udføre autentificering.
+                    return RedirectToPage("/Index"); //Gør at når man logget ind vil man blive sendt hen til forside.
 
                 }
 
             }
 
-            return Page();
+            return Page(); //Ellers vil man return til siden igen hvis programmet ikke kan finde en bruger i listen.
         }
     }
 }
--- Semesterprojekt/Pages/Login/LogUd.cshtml.cs
+++ Semesterprojekt/Pages/Login/LogUd.cshtml.cs
@@ -7,12 +7,18 @@ namespace Semesterprojekt.Pages.Login
 {
     public class LogUdModel : PageModel
     {
+
+
+        //async den kan gør det i baggrund og ikke blokere hovedtråden (det er den som er ansvarlig for at køre vores applikations logik).
+        //hvis vi gør uden async kan hovedtråden bliver inaktiv indtil den er færdig hvilket gør at programmet kan virke frosset eller ikke reagerer fordi den venter på at den er færdig.
+
+        //Task<IActionResult> går ude på at Task viser at den skal returnerer en opgave af type IActionResult.
         public async Task<IActionResult> OnGet()
         {
-            LogIndModel.LoggedInUser = null;
+            LogIndModel.LoggedInUser = null; //LoggedInUser bliver sat til null.
 
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToPage("/index");
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme); //HttpContext.SignOutAsync: den logger en ude ved at slette authentication cookie. og CookieAuthenticationDefaults.AuthenticationScheme: fortæller hvad der er brugt til at godkende hvilket er cookie-baseret
+            return RedirectToPage("/index"); //Gør at når man logget ind vil man blive sendt hen til forside.
         }
     }
 }
